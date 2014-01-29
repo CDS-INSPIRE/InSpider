5640f1ee71ccf1ee7af1a2b01b09c648129feac5
Markus Schneider <schneider@occamlabs.de>
2014-01-29 18:40:46 +0100

    More work on mapping of Hydrography

--- theme-hydronode/src/main/java/nl/ipo/cds/etl/theme/hydronode/HydroNode.java
+++ theme-hydronode/src/main/java/nl/ipo/cds/etl/theme/hydronode/HydroNode.java
@@ -18,42 +18,11 @@ public class HydroNode extends PersistableFeature {
 	@Column(name = "inspire_id_local_id")
 	private String inspireIdLocalId;
 
-	@Column(name = "production_facility_id")
-	private String productionFacilityId;
-
-	@Column(name = "thematic_identifier")
-	private String thematicIdentifier;
-
-	@Column(name = "thematic_identifier_scheme")
-	private String thematicIdentifierScheme;
-
 	@Column(name = "geometry")
 	private Geometry geometry;
 
-	@Column(name = "function_activity")
-	private CodeType functionActivity;
-
-	@Column(name = "function_input")
-	private CodeType functionInput;
-
-	@Column(name = "function_output")
-	private CodeType functionOutput;
-
-	@Column(name = "function_description")
-	private String functionDescription;
-
 	@Column(name = "name")
 	private String name;
-
-	@Column(name = "surface_geometry")
-	private Geometry surfaceGeometry;
-
-	@Column(name = "status_type")
-	private CodeType statusType;
-
-	@Column(name = "status_description")
-	private String statusDescription;
-
 
 	@MappableAttribute
 	@CodeSpace("http://www.inspire-provincies.nl/codeList/DatasetTypeCode/ProductionFacility")
@@ -85,124 +54,6 @@ public class HydroNode extends PersistableFeature {
 	@MappableAttribute
 	public void setGeometry(Geometry geometry) {
 		this.geometry = geometry;
-	}
-
-	@MappableAttribute
-	public String getProductionFacilityId() {
-		return productionFacilityId;
-	}
-
-	@MappableAttribute
-	public void setProductionFacilityId(String productionFacilityId) {
-		this.productionFacilityId = productionFacilityId;
-	}
-
-	@MappableAttribute
-	public String getThematicIdentifier() {
-		return thematicIdentifier;
-	}
-
-	@MappableAttribute
-	public void setThematicIdentifier(String thematicIdentifier) {
-		this.thematicIdentifier = thematicIdentifier;
-	}
-
-	@MappableAttribute
-	public String getThematicIdentifierScheme() {
-		return thematicIdentifierScheme;
-	}
-
-	@MappableAttribute
-	public void setThematicIdentifierScheme(String thematicIdentifierScheme) {
-		this.thematicIdentifierScheme = thematicIdentifierScheme;
-	}
-
-	@MappableAttribute
-	@CodeSpace("http://inspire.ec.europa.eu/codeList/EconomicActivityNACEValue")
-	public CodeType getFunctionActivity() {
-		return functionActivity;
-	}
-
-	@MappableAttribute
-	@CodeSpace("http://inspire.ec.europa.eu/codeList/EconomicActivityNACEValue")
-	public void setFunctionActivity(CodeType functionActivity) {
-		this.functionActivity = functionActivity;
-	}
-
-	@MappableAttribute
-	@CodeSpace("http://inspire.ec.europa.eu/codeList/ProductCPAValue")
-	public CodeType getFunctionInput() {
-		return functionInput;
-	}
-
-	@MappableAttribute
-	@CodeSpace("http://inspire.ec.europa.eu/codeList/ProductCPAValue")
-	public void setFunctionInput(CodeType functionInput) {
-		this.functionInput = functionInput;
-	}
-
-	@MappableAttribute
-	@CodeSpace("http://inspire.ec.europa.eu/codeList/ProductCPAValue")
-	public CodeType getFunctionOutput() {
-		return functionOutput;
-	}
-
-	@MappableAttribute
-	@CodeSpace("http://inspire.ec.europa.eu/codeList/ProductCPAValue")
-	public void setFunctionOutput(CodeType functionOutput) {
-		this.functionOutput = functionOutput;
-	}
-
-	@MappableAttribute
-	public String getFunctionDescription() {
-		return functionDescription;
-	}
-
-	@MappableAttribute
-	public void setFunctionDescription(String functionDescription) {
-		this.functionDescription = functionDescription;
-	}
-
-	@MappableAttribute
-	public String getName() {
-		return name;
-	}
-
-	@MappableAttribute
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@MappableAttribute
-	public Geometry getSurfaceGeometry() {
-		return surfaceGeometry;
-	}
-
-	@MappableAttribute
-	public void setSurfaceGeometry(Geometry surfaceGeometry) {
-		this.surfaceGeometry = surfaceGeometry;
-	}
-
-	@MappableAttribute
-	@CodeSpace("http://inspire.ec.europa.eu/codeList/ConditionOfFacilityValue")
-	public CodeType getStatusType() {
-		return statusType;
-	}
-
-	@MappableAttribute
-	@CodeSpace("http://inspire.ec.europa.eu/codeList/ConditionOfFacilityValue")
-	public void setStatusType(CodeType statusType) {
-		this.statusType = statusType;
-	}
-
-	@MappableAttribute
-	public String getStatusDescription() {
-		return statusDescription;
-	}
-
-	@MappableAttribute
-	public void setStatusDescription(String statusDescription) {
-		this.statusDescription = statusDescription;
 	}
 
 }
--- theme-watercourselink/src/main/sql/populate-manager-schema-watercourselink.sql
+++ theme-watercourselink/src/main/sql/populate-manager-schema-watercourselink.sql
@@ -4,7 +4,7 @@
 -- Names must match constants defined corresponding ThemeConfig
 insert into manager.thema (id, naam) values ((select nextval('manager.hibernate_sequence')), 'WatercourseLink');
 	
-insert into manager.datasettype (id, naam, thema_id) values ((select nextval('manager.hibernate_sequence')), 'HydroNetworkNodesNL', (select id from manager.thema t where t.naam = 'WatercourseLink'));
+insert into manager.datasettype (id, naam, thema_id) values ((select nextval('manager.hibernate_sequence')), 'WatercourseLinksNL', (select id from manager.thema t where t.naam = 'WatercourseLink'));
 
 insert into manager.bronhouder (id, code, contact_naam, contact_adres, contact_postcode, contact_plaats, naam, contact_emailadres, contact_telefoonnummer, common_name) select nextval('manager.hibernate_sequence'), 'elf', 'ELF Project', '', '', '', 'ELF Project', 'inspire@idgis.nl', '', 'elfproject' where not exists (select * from manager.bronhouder where code = 'elf');
 

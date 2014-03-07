0dc614e83a8e787b64b27196146ec98eeb82c52b
copierrj <reijer.copier@idgis.nl>
2014-03-07 17:39:42 +0100

    catalog metadata: missing xpath expressions added

--- catalog-metadata/src/main/java/nl/ipo/cds/metadata/MetadataManager.java
+++ catalog-metadata/src/main/java/nl/ipo/cds/metadata/MetadataManager.java
@@ -47,7 +47,7 @@ public class MetadataManager {
 		final File f = new File(metadataFolder, documentName);
 		
 		if(!f.exists()) {
-			throw new IllegalArgumentException("Metadata document doens't exists.");
+			throw new IllegalArgumentException("Metadata document doesn't exists.");
 		}
 		
 		try {		
@@ -74,14 +74,16 @@ public class MetadataManager {
 		rewriter.addNamespace("gmd", "http://www.isotc211.org/2005/gmd");
 		rewriter.addNamespace("gco", "http://www.isotc211.org/2005/gco");
 		rewriter.addNamespace("srv", "http://www.isotc211.org/2005/srv");
+		rewriter.addNamespace("gml", "http://www.opengis.net/gml");
 		return rewriter;
 	}
 	
 	protected void updateServiceMetadata(final XMLRewriter rewriter, final String dateTime) throws XPathExpressionException {
-	
+		rewriter.modify("/gmd:MD_Metadata/gmd:identificationInfo/srv:SV_ServiceIdentification/srv:extent/gmd:EX_Extent/gmd:temporalElement/gmd:EX_TemporalExtent/gmd:extent/gml:TimePeriod/gml:begin/gml:TimeInstant/gml:timePosition", dateTime); 
 	}
 	
-	protected void updateDatasetMetadata(final XMLRewriter rewriter, final String dateTime) throws XPathExpressionException {
-		rewriter.modify("/gmd:MD_Metadata/gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:date/gmd:CI_Date[gmd:dateType/gmd:CI_DateTypeCode/@codeListValue = 'revision']/gmd:date/gco:DateTime", dateTime);		
+	protected void updateDatasetMetadata(final XMLRewriter rewriter, final String dateTime) throws XPathExpressionException {		
+		rewriter.modify("/gmd:MD_Metadata/gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:date/gmd:CI_Date[gmd:dateType/gmd:CI_DateTypeCode/@codeListValue = 'revision']/gmd:date/gco:DateTime", dateTime);
+		rewriter.modify("/gmd:MD_Metadata/gmd:identificationInfo/gmd:MD_DataIdentification/gmd:extent/gmd:EX_Extent/gmd:temporalElement/gmd:EX_TemporalExtent/gmd:extent/gml:TimePeriod/gml:begin/gml:TimeInstant/gml:timePosition", dateTime);
 	}
 }
--- catalog-metadata/src/test/java/nl/ipo/cds/metadata/MetadataManagerTest.java
+++ catalog-metadata/src/test/java/nl/ipo/cds/metadata/MetadataManagerTest.java
@@ -1,16 +1,31 @@
 package nl.ipo.cds.metadata;
 
+import java.io.InputStream;
+
 import nl.ipo.cds.metadata.MetadataManager;
 import nl.ipo.cds.metadata.XMLRewriter;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+
 public class MetadataManagerTest {
+	
+	MetadataManager manager = new MetadataManager();
+	
+	private XMLRewriter getRewriter(String documentName) throws Exception {
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream("nl/ipo/cds/metadata/" + documentName);
+		assertNotNull(inputStream);
+		
+		XMLRewriter rewriter = manager.createRewriter(inputStream);
+		assertNotNull(rewriter);
+		
+		return rewriter;
+	}
 
 	@Test
-	public void testUpdate() throws Exception {
-		MetadataManager manager = new MetadataManager();
-		XMLRewriter rewriter = manager.createRewriter(getClass().getClassLoader().getResourceAsStream("nl/ipo/cds/metadata/protectedSites.xml"));
-		manager.updateDatasetMetadata(rewriter, "");
+	public void testUpdate() throws Exception {		
+		manager.updateDatasetMetadata(getRewriter("protectedSites.xml"), "");
+		manager.updateServiceMetadata(getRewriter("protectedSitesView.xml"), "");
 	}
 }

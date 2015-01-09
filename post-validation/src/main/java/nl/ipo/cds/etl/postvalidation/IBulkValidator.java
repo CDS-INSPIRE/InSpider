1a6bc724d98efaa3a197e49d25c3bfbe0a452242
Stefan van Wouw <stefan.van.wouw@geodan.nl>
2015-01-09 14:11:57 +0100

    Test stub + jdbc stub

--- post-validation/src/main/java/nl/ipo/cds/etl/postvalidation/IBulkValidator.java
+++ post-validation/src/main/java/nl/ipo/cds/etl/postvalidation/IBulkValidator.java
@@ -3,7 +3,8 @@ package nl.ipo.cds.etl.postvalidation;
 /**
  * Responsible for validating that geometries do not overlap.
  */
+
 public interface IBulkValidator {
 
-    public void overlapValidation();
+    public void overlapValidation(String jdbcConnectStr);
 }
---
+++ post-validation/src/test/java/nl/ipo/cds/etl/postvalidation/BulkValidatorTest.java
@@ -0,0 +1,18 @@
+package nl.ipo.cds.etl.postvalidation;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@TransactionConfiguration(defaultRollback = true, transactionManager = "transactionManager")
+public class BulkValidatorTest {
+
+    @Test
+    public void testOverlapValidation() throws Exception {
+
+    }
+}
